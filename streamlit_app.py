83bff2fb782e01f6b85e61ba25b39c35d4b311e1
Omar Temsamani <omartemsamani22@gmail.com>
2024-04-18 14:52:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import os.path
 import os
-from tkinter import Image
 import streamlit as st
 from streamlit_extras.add_vertical_space import add_vertical_space 
 os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
